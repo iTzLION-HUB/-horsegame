ac0fca4ad81e2e89587eb50a14bb7ba952e683f9
iTzLION-HUB <83901833+iTzLION-HUB@users.noreply.github.com>
2022-12-15 19:04:25 +0530

    Updated PORT

--- app.js
+++ app.js
@@ -109,4 +109,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log(`Listening on Port ${port}`);
 module.exports = app;
